b90bd3beb9005e85e01aecb8eca2d19fec661c0a
Bamboo Forest <113477194+Mr-Bamboo-Forest@users.noreply.github.com>
2024-04-12 23:08:48 +1000

    compound interest

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 function simpleInterestCalc(){
-    var principle = Number(document.getElementById("simpleInterstPrinciple").value)
+    var Principal = Number(document.getElementById("simpleInterstPrincipal").value)
     var rate = Number(document.getElementById("simpleInterstRate").value)/100
     var time = Number(document.getElementById("simpleInterstTime").value)
-    document.getElementById("simpleInterstAnswer").innerText= ("The interest is $")+comma((principle*rate*time));
-    document.getElementById("simpleInterstAnswer2").innerText= ("The total amount is $")+comma((principle*rate*time+principle));
-}
+    document.getElementById("simpleInterstAnswer").innerText= ("The interest is $")+comma((Principal*rate*time));
+    document.getElementById("simpleInterstAnswer2").innerText= ("The total amount is $")+comma((Principal*rate*time+Principal));
+};
+function compoundInterestCalc(){
+    var Principal = Number(document.getElementById("compoundInterestPrincipal").value)
+    var rate = Number(document.getElementById("compoundInterestRate").value)/100
+    var time = Number(document.getElementById("compoundInterestTime").value)
+    var cinterest = Number(((Principal*((1+rate)**time)) - Principal).toFixed(2))
+    document.getElementById("compoundInterestAnswer").innerText= ("The interest (2 d.p.) is $")+comma((cinterest));
+    document.getElementById("compoundInterestAnswer2").innerText= ("The total (2 d.p.) amount is $")+comma((cinterest+Principal));
+};
 //loan calculator
 function calculateloan() {
     var amount = Number(document.getElementById("loanamount").value)
@@ -13,7 +21,7 @@ function calculateloan() {
     var interest = (amount * (rate * 0.01)) / months;
     var totalamount = ((amount / months) + interest).toFixed(2);
     var totalamountwithoutinterest = ((amount / months)).toFixed(2);
-    document.getElementById("interest").innerText = ("Interest is $") + interest.toFixed(2);
+    document.getElementById("interest").innerText = ("Interest (2 d.p.) is $") + interest.toFixed(2);
     document.getElementById("monthlypayment").innerText = ("Monthly payment (2 d.p. and with interest) is $") + totalamount;
     document.getElementById("monthlypaymentwithoutinterest").innerText = ("Monthly payment (2 d.p. and without interest) is $") + totalamountwithoutinterest;
 };
